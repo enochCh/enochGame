e8f2b9432ef4a430cc06bff50e11f3b56b6db830
enochCH <enochc2029@student.cis.edu.hk>
2024-02-28 15:57:12 +0800

    Initial Commit

---
+++ enochGame/enochGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  enochGameApp.swift
+//  enochGame
+//
+//  Created by Enoch Chiu on 28/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct enochGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
